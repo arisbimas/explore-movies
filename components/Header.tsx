657131526ba5530f8f121594b9447cc5d5749277
Aris Bimas Nugroho <arisbimas2@gmail.com>
2026-09-02 23:35:49 +0700

    style(header): max-w

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Header() {
     return (
         <header className="bg-surface">
-            <div className="w-full mx-auto max-w-360 py-4 px-4 sm:px-3 lg:px-8">
+            <div className="w-full mx-auto max-w-7xl py-4 px-4 sm:px-3 lg:px-8">
                 <Link href="/" className="font-display text-xl font-bold text-primary">
                     Movie Explorer
                 </Link>
